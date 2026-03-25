6d8f9bf48dfb57cba88a2c6d572c7ec6c3ba20ee
Rakin Rahman <rakinhacker@gmail.com>
2026-03-26 01:10:53 +0600

    feat: add log file path

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,10 @@
 import os from "os";
 import path from "path";
 
-const CONFIG_DIR = path.join(os.homedir(), ".config", "aiub-notifier");
-const PREV_LINK_FILE = path.join(CONFIG_DIR, "prev-link.txt");
+const DATA_DIR = path.join(os.homedir(), ".local", "share", "aiub-notifier");
+const LOG_FILE = path.join(DATA_DIR, "logs", `pino-${process.pid}-log`);
+const PREV_LINK_FILE = path.join(DATA_DIR, "prev-link.txt");
 const AIUB_ENDPOINT = "https://www.aiub.edu";
 const AIUB_NOTICES_URL = AIUB_ENDPOINT + "/category/notices";
 
-export { AIUB_ENDPOINT, AIUB_NOTICES_URL, PREV_LINK_FILE };
+export { DATA_DIR, LOG_FILE, PREV_LINK_FILE, AIUB_ENDPOINT, AIUB_NOTICES_URL };
