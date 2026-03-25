98b2699e0ba8d09de8bdcca197f31d941b523814
Rakin Rahman <rakinhacker@gmail.com>
2026-03-26 00:57:48 +0600

    feat: add config directory

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
+import os from "os";
+import path from "path";
+
+const CONFIG_DIR = path.join(os.homedir(), ".config", "aiub-notifier");
+const PREV_LINK_FILE = path.join(CONFIG_DIR, "prev-link.txt");
 const AIUB_ENDPOINT = "https://www.aiub.edu";
 const AIUB_NOTICES_URL = AIUB_ENDPOINT + "/category/notices";
 
-export { AIUB_ENDPOINT, AIUB_NOTICES_URL };
+export { AIUB_ENDPOINT, AIUB_NOTICES_URL, PREV_LINK_FILE };
